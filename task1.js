a3d0aa3c55caad9ca68d4904135d7efe77d17b42
Sridhar <sridhar.career.2001@gmail.com>
2026-08-25 23:00:14 +0530

    first commit

---
+++ task1.js
@@ -0,0 +1,18 @@
+var studentName = "Sridhar";
+var studentAge = 18;
+var studentMark=100;
+console.log(studentName);
+console.log(studentAge);
+console.log(studentMark);
+
+var userName = prompt();
+alert(userName);
+
+let userAge = prompt();
+console.log(userAge);
+
+let userQuestion = "Do you know how to write the program ?";
+confirm(userQuestion);
+
+var userName = prompt();
+document.writeln(userName);
