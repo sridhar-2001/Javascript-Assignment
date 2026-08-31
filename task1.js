5b1ffe1f82aabb14181ee28f67334f7ca2c6cc76
SRIDHAR <sridhar.career.2001@gmail.com>
2026-08-31 23:08:53 +0530

    Add student details and console log
    Added variable declarations and console log for student details.

--- task1.js
+++ task1.js
@@ -1,3 +1,52 @@
+// 1. Used to store the values.
+// 2. (i)var, (ii)let, (iii)const
+// 3. var a = 1;
+// 4. let b = 2;
+// 5. const c = 2;
+// 6. Declaration means creating a variable.
+// 7. it means assign Value.
+// 8. it means changing the value of already assigned value. 
+// 9. it means declaring the same variable again. 
+// 10. var 
+// 11. var, let 
+// 12. const 
+// 13. let age is Declaration, age=25 is initilaization. 
+// 14. 100
+// 15. number=200;
+// 16. 10
+// 17. 20
+// 18. 30 
+// 19. var name="John";
+// 20. var price = 500;
+// 21. var price = 500;
+//     price = 1000;
+// 22. 100 
+// 23. yes
+// 24. yes
+// 25. var name = "Sri"
+//     var name = "Parthi"
+// 26. let age = 25;
+// 27. 30
+// 28. yes
+// 29. No 
+// 30. Cannot redeclare, it shows error
+// 31. let city = "chennai";
+// 32. city = "Salem";
+// 33. 50
+// 34. let salary = 25000;
+// 35. salary = 30000;
+// 36. const pi = 3.14;
+// 37. No 
+// 38. No 
+// 39. Reassign is not possible in const 
+// 40. in this code they reasign the value in constant. so it shows error. 
+// 41. const country = "India";
+// 42. 100
+// 43. const 
+// 44. let can reasign value but cannot redeclare in const we can't do both redeclare and reasssign
+// 45. In var we can do both redeclare and reasssign but in let we can do only reassign .
+
+
 var studentName = "Sridhar";
 var studentAge = 18;
 var studentMark=100;
